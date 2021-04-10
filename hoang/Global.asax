30c07a448734453d63af60a9a2d93918be18cd88
sinhvien <sinhvien>
2021-04-10 10:21:10 +0700

    ft2

---
+++ hoang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hoang.MvcApplication" Language="C#" %>
---
+++ hoang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hoang.Startup))]
+namespace hoang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
